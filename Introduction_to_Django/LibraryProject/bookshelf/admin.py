1bf9851e39abf50f6c55bd46a7d411970dba8f11
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-13 22:20:49 +0300

    Add file edits

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,8 +7,9 @@ admin.site.register(Author)
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_filter =('last_name', 'first_name')
-    fields = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author','publication_year')
+    search_fields = ('author', 'publication_year')
