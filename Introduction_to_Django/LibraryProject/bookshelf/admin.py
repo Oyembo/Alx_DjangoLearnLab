26dbb58e4fcf23ab3797ee2e1e0ae7705bbd69c8
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-13 22:15:50 +0300

    Add file edits

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book, Author
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display =('last_name', 'first_name')
+    fields = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_date')
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 class Book(models.Model):
-    title = models.CharField "max_length", "200")
+    title = models.CharField (max_length=200)
     author= models.ForeignKey
     publication_date = models.DateField
 class Meta:
