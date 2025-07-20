97530c2707287c48b1127153eed3edc8f1c41164
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-20 15:31:38 +0300

    Add recent file edits

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    class Meta:
-        ordering = ['first_name', 'last_name']
+    name = models.CharField(max_length=100)     
     def __str__(self):
-        return f'{self.first_name}, '{self.last_name}'
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -18,11 +15,10 @@ class Library(models.Model):
     books = models.ManytoManyField(Book,related_name= 'Library')
 
 class Librarian(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     library = models.OneToOneField(Library)
     class Meta:
-        ordering = ['first_name', 'last_name', 'library']
+        ordering = ['name', 'library']
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}, {self.library}'
+        return f'{self.name}, {self.library}'
 
