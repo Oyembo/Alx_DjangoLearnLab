59b31e5507d4c70ebf87f58fcb45893e3e090163
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-20 15:50:16 +0300

    Add recent file edits

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Author, Book, Library, Librarian
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    search_fields = ('name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    list_filter = ('title', 'author') # Search book by author and book title
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'books')
+    list_filter = ('books') #Search books in library
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('name', 'library')
+    search_fields = ('name', 'library')
