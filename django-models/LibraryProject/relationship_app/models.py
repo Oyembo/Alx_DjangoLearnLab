ba553f30d8235614140b7c490f0a63a3f78cd074
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-20 21:34:06 +0300

    add edits

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -11,8 +11,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=50
     choices = UserRole.choices
-    default = UserRole.MEMBER
+    default = UserRole.ADMIN
     )
+    def __str__(self):
+        return f"{self.user.username}'s Profile ({self.get_role_display()})"
 
 class Author(models.Model):
     name = models.CharField(max_length=100)   
