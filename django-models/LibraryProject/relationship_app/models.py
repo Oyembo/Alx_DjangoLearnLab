e6c9fd2c8c677933aad4f2b29c1652ee11236a3b
Oyembo Peninah <aumaoyembo@gmail.com>
2025-07-20 21:04:41 +0300

    Add recent file edits

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserRole(models.TextChoices):
+    ADMIN = 'admin', 'Admin'
+    LIBRARIAN = 'librarian', 'Librarian'
+    MEMBER = 'member', 'Member'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=50
+    choices = UserRole.choices
+    default = UserRole.MEMBER
+    )
+
 class Author(models.Model):
     name = models.CharField(max_length=100)   
 
