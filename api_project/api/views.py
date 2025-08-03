a91bbf6342070594d655982feaf58cd70dcedddb
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-03 22:07:56 +0300

    add edits

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,4 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
