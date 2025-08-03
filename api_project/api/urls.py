a1b4c2d3358d345980c19d128fbde9dc445d053b
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-03 21:47:30 +0300

    add edits

---
+++ api_project/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import BookList
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # This line routes requests to your API
+]
