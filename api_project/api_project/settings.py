9e2e9111e1fa12735e5e480e8f6cca2c894e9866
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-03 22:33:17 +0300

    add edits

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -69,6 +69,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 WSGI_APPLICATION = 'api_project.wsgi.application'
 
 
