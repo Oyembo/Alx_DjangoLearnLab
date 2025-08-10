2175ac513a8a992aac6f68240e31239508a52bf1
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-10 19:28:56 +0300

    add edits

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from bookshelf.models import Book
