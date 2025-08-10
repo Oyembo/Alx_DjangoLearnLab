b26b3e5c693dccefd6da83e9eb73caf4d9d5e295
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-10 21:10:13 +0300

    add changes

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book_author')
-    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name} published in {self.publication_year}"
