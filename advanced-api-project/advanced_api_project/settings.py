ff0df356fe34c1d9f3fdce2b59161dd1a2cd5a22
Oyembo Peninah <aumaoyembo@gmail.com>
2025-08-10 22:09:11 +0300

    Securely load SECRET_KEY from env

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aw8hi@6nkbe$iw7b40p$71_w(%0^)%4p$_ewaomkbs@se4t$2a'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
